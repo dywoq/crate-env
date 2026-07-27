fa022107821ec8356850f9a6fa8b47649b97d166
dywoq <dywoq.contact@gmail.com>
2026-07-27 12:48:36 +0300

    lib.rs: Make fields public
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
 // Copyright 2026 dywoq - Apache License 2.0
 // https://github.com/dywoq/crate-env
 
-/// Represents a crate version information.
+/// Represents the crate version information.
 pub struct Version {
-    major: Option<String>,
-    minor: Option<String>,
-    patch: Option<String>,
-    pre: Option<String>,
-    full: Option<String>,
+    pub major: Option<String>,
+    pub minor: Option<String>,
+    pub patch: Option<String>,
+    pub pre: Option<String>,
+    pub full: Option<String>,
 }
 
-/// Represents a crate information, which copies the environment variables from
+/// Represents the crate information, which copies the environment variables from
 /// [this page](https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates).
 pub struct Information {
-    cargo_path: Option<String>,
-    manifest_dir: Option<String>,
-    manifest_path: Option<String>,
-    version: Version,
-    authors: Option<Vec<String>>,
-    name: Option<String>,
-    description: Option<String>,
-    homepage: Option<String>,
-    repository: Option<String>,
-    license: Option<String>,
-    license_file: Option<String>,
-    rust_version: Option<String>,
-    readme: Option<String>,
-    crate_name: Option<String>,
-    bin_name: Option<String>,
-    out_dir: Option<String>,
-    primary_package: Option<String>,
-    target_tmpdir: Option<String>,
+    pub cargo_path: Option<String>,
+    pub manifest_dir: Option<String>,
+    pub manifest_path: Option<String>,
+    pub version: Version,
+    pub authors: Option<Vec<String>>,
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub homepage: Option<String>,
+    pub repository: Option<String>,
+    pub license: Option<String>,
+    pub license_file: Option<String>,
+    pub rust_version: Option<String>,
+    pub readme: Option<String>,
+    pub crate_name: Option<String>,
+    pub bin_name: Option<String>,
+    pub out_dir: Option<String>,
+    pub primary_package: Option<String>,
+    pub target_tmpdir: Option<String>,
 }
